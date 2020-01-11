c0a8ccb8c3fa9f4a722511cd672d7de3101b364c
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-01-11 15:21:12 +0000

    Added HttpClient and Module

--- src/app/services/session/session.service.ts
+++ src/app/services/session/session.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class SessionService {
   user: IUser;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.user = {
       username: '',
       name: '',
