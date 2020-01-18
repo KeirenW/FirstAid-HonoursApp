a3995c492763cb4a08446fb9615d3a0722bf1832
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-01-18 17:31:36 +0000

    Replaced sesion service with auth and firebase services

--- src/app/services/firebase/firebase.service.spec.ts
+++ src/app/services/firebase/firebase.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { SessionService } from './session.service';
+import { FirebaseService } from './firebase.service';
 
-describe('SessionService', () => {
+describe('FirebaseService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: SessionService = TestBed.get(SessionService);
+    const service: FirebaseService = TestBed.get(FirebaseService);
     expect(service).toBeTruthy();
   });
 });
