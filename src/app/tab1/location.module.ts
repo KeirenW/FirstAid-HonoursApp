d831bd1065a47fc98f16bb9a69e5a0e7419a1d21
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-03-02 22:50:54 +0000

    Updated names of tab1 components

--- src/app/tab1/location.module.ts
+++ src/app/tab1/location.module.ts
@@ -3,15 +3,15 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab1Page } from './tab1.page';
+import { LocationPage } from './location.page';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{ path: '', component: LocationPage }])
   ],
-  declarations: [Tab1Page]
+  declarations: [LocationPage]
 })
-export class Tab1PageModule {}
+export class LocationModule {}
