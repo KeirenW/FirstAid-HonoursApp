54c75b8379127df613fb2e09f7b3bd48c934604f
Keiren Waddell <kjwaddell@dundee.ac.uk>
2020-04-22 16:13:21 +0100

    Firebase API key etc. This is okay to submit per: https://github.com/zooniverse/vox/issues/32

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAbAeYCO4sDQJMd_Eu4RNl9kuLwtxWjqjI',
+    authDomain: 'test-f6dc6.firebaseapp.com',
+    databaseURL: 'https://test-f6dc6.firebaseio.com',
+    projectId: 'test-f6dc6',
+    storageBucket: 'test-f6dc6.appspot.com',
+    messagingSenderId: '922596871836',
+    appId: '1:922596871836:web:d3b50e27a9249c2ad1a759'
+  }
 };
 
 /*
